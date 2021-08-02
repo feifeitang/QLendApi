53e3cc1373c6007c57aa0c174f59cadf91119f05
joannetang <joannetang2425@gmail.com>
2021-08-02 13:33:40 +0800

    loanRecord add status

--- QLendApi/Migrations/20210802052828_LoanRecordAddStatus.Designer.cs
+++ QLendApi/Migrations/20210802052828_LoanRecordAddStatus.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QLendApi.Models;
 
 namespace QLendApi.Migrations
 {
     [DbContext(typeof(QLendDBContext))]
-    partial class QLendDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210802052828_LoanRecordAddStatus")]
+    partial class LoanRecordAddStatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
