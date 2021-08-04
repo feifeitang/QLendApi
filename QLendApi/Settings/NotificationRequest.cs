da87a6ab9943b719cb7773bc91e8e260dfceb03e
joannetang <joannetang2425@gmail.com>
2021-08-04 11:59:34 +0800

    first commit

---
+++ QLendApi/Settings/NotificationRequest.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace QLendApi.Settings
+{
+    public class NotificationRequest
+    {
+        public string Text { get; set; }
+        public string Action { get; set; }
+        public string[] Tags { get; set; } = Array.Empty<string>();
+        public bool Silent { get; set; }
+    }
+}
--- QLendApi/Startup.cs
+++ QLendApi/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using PushDemoApi.Services;
+using QLendApi.Dtos;
 using QLendApi.Helpers;
 using QLendApi.Models;
 using QLendApi.Repositories;
@@ -62,6 +64,12 @@ namespace QLendApi
             });
 
             services.AddHealthChecks();
+
+            services.AddSingleton<INotificationService, NotificationHubService>();
+
+            services.AddOptions<NotificationHubOptions>()
+                .Configure(Configuration.GetSection("NotificationHub").Bind)
+                .ValidateDataAnnotations();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
