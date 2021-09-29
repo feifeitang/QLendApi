7f3c2ce803132de50ce92df1d11a649d53aaf0bb
joannetang <joannetang2425@gmail.com>
2021-09-29 14:05:23 +0800

    add originalAmount

---
+++ QLendApi/Migrations/20210929060225_addOriginalAmount.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QLendApi.Migrations
+{
+    public partial class addOriginalAmount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrginalAmount",
+                table: "RepaymentRecord",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrginalAmount",
+                table: "RepaymentRecord");
+        }
+    }
+}
