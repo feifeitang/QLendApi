971c7dbeed6cb2aac278b489753d30fff24071ce
joannetang <joannetang2425@gmail.com>
2021-09-29 10:56:44 +0800

    migration barCodeCreateTime

---
+++ QLendApi/Migrations/20210929025433_storeBarCodeCreateTimeToRepaymentRecord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QLendApi.Migrations
+{
+    public partial class storeBarCodeCreateTimeToRepaymentRecord : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BarCodeCreateTime",
+                table: "RepaymentRecord",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BarCodeCreateTime",
+                table: "RepaymentRecord");
+        }
+    }
+}
