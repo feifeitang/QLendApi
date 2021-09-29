516db051baf416c182af70d768d8c890b22996f3
joannetang <joannetang2425@gmail.com>
2021-09-29 23:31:07 +0800

    Fix naming error

--- QLendApi/Models/RepaymentRecord.cs
+++ QLendApi/Models/RepaymentRecord.cs
@@ -23,7 +23,7 @@ namespace QLendApi.Models
         public int State { get; set; }
         public string LoanNumber { get; set; }
         public DateTime? BarCodeCreateTime { get; set; }
-        public int OrginalAmount { get; set; }
+        public int OriginalAmount { get; set; }
         
 
         public virtual LoanRecord LoanNumberNavigation { get; set; }
