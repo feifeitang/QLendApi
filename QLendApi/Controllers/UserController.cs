d53829cb74a2d6f6db412b8385b660a2e93406c7
Grace <107306019@g.nccu.edu.tw>
2022-01-22 18:09:54 +0800

    revise income number

--- QLendApi/Controllers/UserController.cs
+++ QLendApi/Controllers/UserController.cs
@@ -1133,8 +1133,9 @@ namespace QLendApi.Controllers
 
         private int GenerateIncomeNumber(string arcNumber)
         {
-            // Random rnd = new Random();  
-            int number = int.Parse(DateTime.UtcNow.ToString("yy") + arcNumber.Substring(5,2) + string.Format("{0:d2}", sn) + arcNumber.Substring(8,2));
+            Random rnd = new Random();  
+            int randomNum = rnd.Next(10, 99);
+            int number = int.Parse(DateTime.UtcNow.ToString("yy") + arcNumber.Substring(5,1) + randomNum + arcNumber.Substring(7,3));
 
            // int number = int.Parse(DateTime.UtcNow.ToString("yyddHH") + string.Format("{0:d4}", sn));
             sn++;
